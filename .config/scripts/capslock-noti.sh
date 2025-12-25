eebd204b168499ec86ddb37c5af7fea5892fc9d6
vijaygudduri <61739941+vijaygudduri@users.noreply.github.com>
2025-12-25 08:13:10 +0530

    Update capslock-noti.sh

--- .config/scripts/capslock-noti.sh
+++ .config/scripts/capslock-noti.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
-sleep 0.15
 
-DATA=$(hyprctl devices -j)
+CAPS_LED=$(ls /sys/class/leds | grep -i '::capslock' | head -n1)
+[ -z "$CAPS_LED" ] && exit 1
 
-if echo "$DATA" | jq -e '.keyboards[] | select(.capsLock == true)' > /dev/null; then
+LED_PATH="/sys/class/leds/$CAPS_LED/brightness"
+
+# read initial state
+old=$(cat "$LED_PATH")
+
+# wait up to ~300ms for it to change
+for _ in {1..15}; do
+    sleep 0.02
+    new=$(cat "$LED_PATH")
+    [ "$new" != "$old" ] && break
+done
+
+if [ "$new" = "1" ]; then
     msg="Caps Lock: ON"
 else
     msg="Caps Lock: OFF"
 fi
 
-# -i sets the icon
-# -t 3000 sets duration to 3 seconds
-notify-send -i input-keyboard -t 3000 -h string:x-canonical-private-synchronous:caps "Keyboard" "$msg"
+notify-send -i input-keyboard -t 3000 \
+  -h string:x-canonical-private-synchronous:caps \
+  "Keyboard" "$msg"
